563015ec134f93def03691ed5c8d04121ec8e1b4
meowbot24 <ceciliawong@Cecilias-MacBook-Air.local>
2016-06-18 17:36:19 -0400

    Push to GitHub
    commitz.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+angular.module("kittyApp", []);
+
+function kittyAppController($scope){
+
+	console.log("deez controllurrr ezzz runninggg");
+
+}
+
+angular
+	.module("kittyApp")
+	.controller("kittyAppController", kittyAppController)
